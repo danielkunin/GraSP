ebfbcdc03e1db6d957800f140e31363a3d4de92f
danielkunin <danielkunin@users.noreply.github.com>
2020-08-08 17:12:24 -0700

    update alg

--- pruner/SynFlow.py
+++ pruner/SynFlow.py
@@ -3,47 +3,59 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-
 import copy
 import types
 
+def score(model, dataloader, device):
+    @torch.no_grad()
+    def linearize(model):
+        signs = {}
+        for name, param in model.state_dict().items():
+            signs[name] = torch.sign(param)
+            param.abs_()
+        return signs
 
-def GraSP_fetch_data(dataloader, num_classes, samples_per_class):
-    datas = [[] for _ in range(num_classes)]
-    labels = [[] for _ in range(num_classes)]
-    mark = dict()
-    dataloader_iter = iter(dataloader)
-    while True:
-        inputs, targets = next(dataloader_iter)
-        for idx in range(inputs.shape[0]):
-            x, y = inputs[idx:idx+1], targets[idx:idx+1]
-            category = y.item()
-            if len(datas[category]) == samples_per_class:
-                mark[category] = True
-                continue
-            datas[category].append(x)
-            labels[category].append(y)
-        if len(mark) == num_classes:
-            break
+    @torch.no_grad()
+    def nonlinearize(model, signs):
+        for name, param in model.state_dict().items():
+            param.mul_(signs[name])
+    
+    signs = linearize(model)
 
-    X, y = torch.cat([torch.cat(_, 0) for _ in datas]), torch.cat([torch.cat(_) for _ in labels]).view(-1)
-    return X, y
+    (data, _) = next(iter(dataloader))
+    input_dim = list(data[0,:].shape)
+    input = torch.ones([1] + input_dim).to(device)
+    output = model(input)
+    torch.sum(output).backward()
+
+    scores = dict()
+    old_modules = list(model.modules())
+    for idx, layer in enumerate(model.modules()):
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            scores[old_modules[idx]] = torch.clone(layer.weight.data * layer.weight.grad).detach().abs_()
+
+    nonlinearize(model, signs)
+
+    return scores
+
+def mask(scores, sparsity):
+    global_scores = torch.cat([torch.flatten(v) for v in scores.values()])
+    k = int((1.0 - sparsity) * global_scores.numel())
+    keep_masks = dict()
+    if not k < 1:
+        threshold, _ = torch.kthvalue(global_scores, k)
+        for mask, score in scores.items():
+            zero = torch.tensor([0.]).to(mask.device)
+            one = torch.tensor([1.]).to(mask.device)
+            keep_masks[m] = torch.where(score <= threshold, zero, one)
+    return keep_masks
 
 
-def count_total_parameters(net):
-    total = 0
-    for m in net.modules():
-        if isinstance(m, (nn.Linear, nn.Conv2d)):
-            total += m.weight.numel()
-    return total
-
-
-def count_fc_parameters(net):
-    total = 0
-    for m in net.modules():
-        if isinstance(m, (nn.Linear)):
-            total += m.weight.numel()
-    return total
+def apply_mask(model, masks):
+    old_modules = list(model.modules())
+    for idx, layer in enumerate(model.modules()):
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            layer.weight.mul_(masks[old_modules[idx]])
 
 
 def SynFlow(net, ratio, train_dataloader, device, num_classes=10, samples_per_class=25, num_iters=1, T=200, reinit=True):
@@ -68,55 +80,10 @@ def SynFlow(net, ratio, train_dataloader, device, num_classes=10, samples_per_cl
     # Prune model
     epochs = 100
     for epoch in tqdm(range(epochs)):
-        pruner.apply_mask()
-        pruner.score(model, loss, dataloader, device)
-        if linear_schedule:
-            sparse = 1.0 - (1.0 - sparsity)*((epoch + 1) / epochs) # Linear
-        else:
-            sparse = sparsity**((epoch + 1) / epochs) # Exponential
-        pruner.mask(sparse, scope)
+        scores = score(model, train_dataloader, device)
+        sparsity = keep_ratio**((epoch + 1) / epochs) # Exponential
+        masks = mask(scores, sparsity)
+        apply_mask(model, masks)
 
-
-    @torch.no_grad()
-    def linearize(model):
-        signs = {}
-        for name, param in model.state_dict().items():
-            signs[name] = torch.sign(param)
-            param.abs_()
-        return signs
-
-    @torch.no_grad()
-    def nonlinearize(model, signs):
-        for name, param in model.state_dict().items():
-            param.mul_(signs[name])
-    
-    signs = linearize(net)
-
-    (data, _) = next(iter(train_dataloader))
-    input_dim = list(data[0,:].shape)
-    input = torch.ones([1] + input_dim).to(device)
-    output = net(input)
-    torch.sum(output).backward()
-
-    scores = dict()
-    old_modules = list(old_net.modules())
-    for idx, layer in enumerate(net.modules()):
-        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
-            grads[old_modules[idx]] = torch.clone(layer.weight.data * layer.weight.grad).detach().abs_()
-
-    nonlinearize(net, signs)
-
-
-    global_scores = torch.cat([torch.flatten(v) for v in self.scores.values()])
-    k = int((1.0 - sparsity) * global_scores.numel())
-    keep_masks = dict()
-    if not k < 1:
-        threshold, _ = torch.kthvalue(global_scores, k)
-        for mask, score in grads.items():
-            zero = torch.tensor([0.]).to(mask.device)
-            one = torch.tensor([1.]).to(mask.device)
-            keep_masks[m] = torch.where(score <= threshold, zero, one)
-
-    print(torch.sum(torch.cat([torch.flatten(x == 1) for x in keep_masks.values()])))
 
     return keep_masks
